657ad708dfb8ab4eb8a709cc0c4f2eec858891cf
rrt <rrt_gogs@liveintellect.org>
2021-06-06 19:52:41 -0600

    Added an alternative set of CLI params using drive letters for windows.

--- README.md
+++ README.md
@@ -23,10 +23,20 @@ target directory into an app directory somewhere.
   * e.g. (on Windows) **unzip -d c:\javaapps\copieddirdatefixer target\CopiedDirDateFixer...distribution.zip**
     * Note: Use the actual name of the zip file created by the Maven build.
 * Run the jar in JRE/Java
-  * Test... **java -jar c:\javaapps\copieddirdatefixer\CopiedDirDateFixer.jar
-     -sourceBaseDirectoryPath d:/orig_dir_w_correct_dates
-     -targetBaseDirectoryPath g:/copy_of_dir_w_wrong_dates**
-  * Do it... **java -jar c:\javaapps\copieddirdatefixer\CopiedDirDateFixer.jar
-     -commitMode true
-     -sourceBaseDirectoryPath d:/orig_dir_w_correct_dates
-     -targetBaseDirectoryPath g:/copy_of_dir_w_wrong_dates**     
+  * Option 1 - Full paths
+     * Test... **java -jar c:\javaapps\copieddirdatefixer\CopiedDirDateFixer.jar
+     --sourceBaseDirectoryPath d:/orig_dir_w_correct_dates
+     --targetBaseDirectoryPath g:/copy_of_dir_w_wrong_dates**
+     * Do it... **java -jar c:\javaapps\copieddirdatefixer\CopiedDirDateFixer.jar
+     --commitMode true
+     --sourceBaseDirectoryPath d:/orig_dir_w_correct_dates
+     --targetBaseDirectoryPath g:/copy_of_dir_w_wrong_dates**     
+  * Option 2 - Drives and a common subdirectory (Windows only)
+     * Test... **java -jar c:\javaapps\copieddirdatefixer\CopiedDirDateFixer.jar
+     --sourceDrive d --targetDrive g --subdirectory subdirectory_named_alike**
+     * Do it... **java -jar c:\javaapps\copieddirdatefixer\CopiedDirDateFixer.jar
+     --commitMode true 
+     --sourceDrive d --targetDrive g --subdirectory subdirectory_named_alike**
+* Note: There are also single-letter short names for each param/option.  Run
+without arguments to show usage info.
+  * **java -jar c:\javaapps\copieddirdatefixer\CopiedDirDateFixer.jar**               
--- src/main/java/org/liveintellect/standaloneapps/copieddirdatefixer/CopiedDirDateFixer.java
+++ src/main/java/org/liveintellect/standaloneapps/copieddirdatefixer/CopiedDirDateFixer.java
@@ -4,7 +4,9 @@ import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionGroup;
 import org.apache.commons.cli.Options;
+import org.apache.commons.cli.UnrecognizedOptionException;
 
 import java.io.File;
 import java.io.FilenameFilter;
@@ -23,10 +25,24 @@ import java.util.Set;
 public class CopiedDirDateFixer {
 
     private static final String OPTION_LONG_SOURCE_BASE_DIRECTORY_PATH = "sourceBaseDirectoryPath";
-
+    private static final String OPTION_SHORT_SOURCE_BASE_DIRECTORY_PATH = "s";
     private static final String OPTION_LONG_TARGET_BASE_DIRECTORY_PATH = "targetBaseDirectoryPath";
+    private static final String OPTION_SHORT_TARGET_BASE_DIRECTORY_PATH = "t";
+
+    /**
+     * This option set only makes sense in Windows where drives have letters, but it
+     * allows repeated commands to be edited more easily when working through directories
+     * at the root level of two drives.
+     */
+    private static final String OPTION_LONG_SOURCE_DRIVE = "sourceDrive";
+    private static final String OPTION_SHORT_SOURCE_DRIVE = "a";
+    private static final String OPTION_LONG_TARGET_DRIVE = "targetDrive";
+    private static final String OPTION_SHORT_TARGET_DRIVE = "b";
+    private static final String OPTION_LONG_SUBDIRECTORY_NAME = "subdirectoryName";
+    private static final String OPTION_SHORT_SUBDIRECTORY_NAME = "n";
 
     private static final String OPTION_LONG_COMMIT_MODE = "commitMode";
+    private static final String OPTION_SHORT_COMMIT_MODE = "c";
 
     private final File sourceBaseDirectoryPath;
     private final File targetBaseDirectoryPath;
@@ -37,6 +53,14 @@ public class CopiedDirDateFixer {
     private static final DateFormat TARGET_DATE_STRING_FORMAT = new SimpleDateFormat(
             TARGET_DATE_STRING_TEMPLATE);
 
+    /**
+     * Params used to specify the full path to the source and the full path to the
+     * target.
+     *
+     * @param sourceBaseDirectoryPath full path to the source directory
+     * @param targetBaseDirectoryPath full path to the target directory
+     * @param commitMode
+     */
     public CopiedDirDateFixer(File sourceBaseDirectoryPath,
             File targetBaseDirectoryPath, boolean commitMode) {
         this.sourceBaseDirectoryPath = sourceBaseDirectoryPath;
@@ -44,6 +68,22 @@ public class CopiedDirDateFixer {
         this.targetBaseDirectoryPath = targetBaseDirectoryPath;
         System.out.println("targetBaseDirectoryPath = " + targetBaseDirectoryPath);
         this.commitMode = commitMode;
+    }
+
+    /**
+     * Alternate set of params that are used to build the same directory path
+     * from two "drive letters" on windows.  Most other filesystems use path
+     * mount points so this probably ONLY makes sense in windows.
+     * @param sourceDriveLetter just the letter of the source drive (case doesn't seem to matter)
+     * @param targetDriveLetter just the letter of the source drive (case doesn't seem to matter)
+     * @param subdirectoryName the name of the subdirectory to use on both source and target drives
+     * @param commitMode true if the changes should actually be made
+     */
+    public CopiedDirDateFixer(String sourceDriveLetter, String targetDriveLetter, String subdirectoryName,
+                              boolean commitMode) {
+        this(new File(sourceDriveLetter + ":/" + subdirectoryName),
+                new File(targetDriveLetter + ":/" + subdirectoryName),
+                commitMode);
     }
 
     private class DirectoryFilenameFilter implements FilenameFilter {
@@ -88,7 +128,7 @@ public class CopiedDirDateFixer {
                 // Only report and/or change dates if something is actually different, otherwise leave it alone
                 if (lastModMismatch || createdMismatch) {
                     System.out.println(targetDirectoryFile.getAbsolutePath() +
-                            ((commitMode) ? " overwritten " : " mismatched ") + " from file dates on source directory: " + sourceDirectoryFile.getAbsolutePath()
+                            ((commitMode) ? " overwritten" : " mismatched") + " from file dates on source directory: " + sourceDirectoryFile.getAbsolutePath()
                             + ((lastModMismatch) ? "\n     lastModified - S: " + sourceDirectoryLastModifiedDateText
                                 + " -> " + ((commitMode) ? "overwrites" : "would overwrite")
                                 + " -> T: " + targetDirectoryLastModifiedDateText : "")
@@ -150,13 +190,40 @@ public class CopiedDirDateFixer {
     }
 
     public static void main(String[] args) throws Throwable {
-        Options options = getCommandLineOptions();
-        CommandLine cli = new BasicParser().parse(options, args);
-        if (cli.hasOption(OPTION_LONG_SOURCE_BASE_DIRECTORY_PATH) && cli.hasOption(OPTION_LONG_TARGET_BASE_DIRECTORY_PATH)) {
+        Options pathsOptions = getPathsCommandLineOptions();
+        Options drivesOptions = getDrivesCommandLineOptions();
+        CommandLine pathsCli = null;
+        try {
+            pathsCli = new BasicParser().parse(pathsOptions, args);
+        } catch(UnrecognizedOptionException ex) {
+            // ignore - this could happen if the drive options are passed instead
+            System.out.println("Valid options for drivesCli may have caused this: "
+                    + ex.getMessage());
+        }
+        CommandLine drivesCli = null;
+        try {
+            drivesCli = new BasicParser().parse(drivesOptions, args);
+        } catch (UnrecognizedOptionException ex) {
+            // ignore this too when a mismatched combination of options is used
+            // like -s with -n
+            System.out.println("A mixed set of options for drivesCli and pathsCli may have caused this: "
+                + ex.getMessage());
+        }
+        if (pathsCli != null && pathsCli.hasOption(OPTION_SHORT_SOURCE_BASE_DIRECTORY_PATH)
+                && pathsCli.hasOption(OPTION_SHORT_TARGET_BASE_DIRECTORY_PATH)) {
             CopiedDirDateFixer copiedDirDateFixer = new CopiedDirDateFixer(
-                    new File(cli.getOptionValue(OPTION_LONG_SOURCE_BASE_DIRECTORY_PATH)),
-                    new File(cli.getOptionValue(OPTION_LONG_TARGET_BASE_DIRECTORY_PATH)),
-                    new Boolean(cli.getOptionValue(OPTION_LONG_COMMIT_MODE, "false")).booleanValue());
+                    new File(pathsCli.getOptionValue(OPTION_SHORT_SOURCE_BASE_DIRECTORY_PATH)),
+                    new File(pathsCli.getOptionValue(OPTION_SHORT_TARGET_BASE_DIRECTORY_PATH)),
+                    new Boolean(pathsCli.getOptionValue(OPTION_SHORT_COMMIT_MODE, "false")).booleanValue());
+            copiedDirDateFixer.fixTargetDirectoryDates();
+        } else if (drivesCli != null && drivesCli.hasOption(OPTION_SHORT_SOURCE_DRIVE)
+                && drivesCli.hasOption(OPTION_SHORT_TARGET_DRIVE)
+                && drivesCli.hasOption(OPTION_SHORT_SUBDIRECTORY_NAME)) {
+            CopiedDirDateFixer copiedDirDateFixer = new CopiedDirDateFixer(
+                    drivesCli.getOptionValue(OPTION_SHORT_SOURCE_DRIVE),
+                    drivesCli.getOptionValue(OPTION_SHORT_TARGET_DRIVE),
+                    drivesCli.getOptionValue(OPTION_SHORT_SUBDIRECTORY_NAME),
+                    new Boolean(drivesCli.getOptionValue(OPTION_SHORT_COMMIT_MODE, "false")).booleanValue());
             copiedDirDateFixer.fixTargetDirectoryDates();
         } else {
             usage();
@@ -165,22 +232,68 @@ public class CopiedDirDateFixer {
 
     private static void usage() {
         new HelpFormatter().printHelp(CopiedDirDateFixer.class.getSimpleName(),
-                getCommandLineOptions());
+                getPathsCommandLineOptions());
+        new HelpFormatter().printHelp(CopiedDirDateFixer.class.getSimpleName(),
+                getDrivesCommandLineOptions());
     }
 
-    private static Options getCommandLineOptions() {
+    /**
+     * Note OptionGroup combines multiple options such that only one of them
+     * may be used.  The only way to group options such that a set
+     * of them must be used together is to create multiple Options objects.
+     * @return paths option set.
+     */
+    private static Options getPathsCommandLineOptions() {
         Options options = new Options();
+
         options.addOption(new Option(
+                OPTION_SHORT_SOURCE_BASE_DIRECTORY_PATH,
                 OPTION_LONG_SOURCE_BASE_DIRECTORY_PATH,
                 true,
                 "The full path to the base directory where the 'source' directories"
                         + ", with the correct file dates, are stored."));
         options.addOption(new Option(
+                OPTION_SHORT_TARGET_BASE_DIRECTORY_PATH,
                 OPTION_LONG_TARGET_BASE_DIRECTORY_PATH,
                 true,
                 "The full path to the base directory where the 'target' directories"
                         + ", with the incorrect file dates, are stored."));
+        // common to both combinations
         options.addOption(new Option(
+                OPTION_SHORT_COMMIT_MODE,
+                OPTION_LONG_COMMIT_MODE,
+                true,
+                "true to indicate that directory dates in the target hierarchy should be altered. defaults to false"));
+        return options;
+    }
+
+    /**
+     * @return drives option set.
+     */
+    private static Options getDrivesCommandLineOptions() {
+        Options options = new Options();
+        options.addOption(new Option(
+                OPTION_SHORT_SOURCE_DRIVE,
+                OPTION_LONG_SOURCE_DRIVE,
+                true,
+                "The drive letter for the 'source' disk/volume."
+                        + "  Used with the " + OPTION_LONG_SUBDIRECTORY_NAME + " parameter."));
+        options.addOption(new Option(
+                OPTION_SHORT_TARGET_DRIVE,
+                OPTION_LONG_TARGET_DRIVE,
+                true,
+                "The drive letter for the 'target' disk/volume."
+                        + "  Used with the " + OPTION_LONG_SUBDIRECTORY_NAME + " parameter."));
+        options.addOption(new Option(
+                OPTION_SHORT_SUBDIRECTORY_NAME,
+                OPTION_LONG_SUBDIRECTORY_NAME,
+                true,
+                "subdirectory to use on both " + OPTION_LONG_SOURCE_DRIVE +
+                        " and " + OPTION_LONG_TARGET_DRIVE));
+
+        // common to both combinations
+        options.addOption(new Option(
+                OPTION_SHORT_COMMIT_MODE,
                 OPTION_LONG_COMMIT_MODE,
                 true,
                 "true to indicate that directory dates in the target hierarchy should be altered. defaults to false"));
